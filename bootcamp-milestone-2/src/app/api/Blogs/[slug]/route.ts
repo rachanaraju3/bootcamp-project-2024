d01a7151d5ebc3299fc18ab287d134f62f6ae14b
Rachana Raju <rachanaraju03@gmail.com>
2024-12-19 18:20:22 -0800

    edited get function to work with vercel

--- bootcamp-milestone-2/src/app/api/Blogs/[slug]/route.ts
+++ bootcamp-milestone-2/src/app/api/Blogs/[slug]/route.ts
@@ -1,18 +1,12 @@
-
 import { NextRequest, NextResponse } from 'next/server'
 import connectDB from '@/database/db'
 import blogSchema from "@/database/blogSchema"
 
-type IParams = {
-		params: {
-			slug: string
-		}
-}
 
 // If { params } looks confusing, check the note below this code block
-export async function GET(req: NextRequest, { params }: IParams) {
+export async function GET(req: NextRequest){
     await connectDB() // function from db.ts before
-		const { slug } = await params // another destructure
+		const slug = req.url.split('/').slice(-1)[0] // another destructure
 
 	   try {
 	        const blog = await blogSchema.findOne({ slug }).orFail()
